7194756a91569a022abe29c28299e0a1d0826fea
jwentong <3317919731@qq.com>
2022-05-16 15:21:01 +0800

    v3

---
+++ src/com/fan/test.java
@@ -0,0 +1,15 @@
+package com.fan;
+
+/**
+ * @program: test-idea-git
+ * @description:
+ * @author: fan
+ * @create: 2022-05-16 15:11
+ **/
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello java v1");
+        System.out.println("hello java v2");
+        System.out.println("hello java v3");
+    }
+}
